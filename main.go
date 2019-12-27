03eab812ada78e7fa5663d6bb98bb3723d5980a7
xavier <xieyuehao@gmail.com>
2019-12-28 09:57:09 +1000

    add array

--- main.go
+++ main.go
@@ -5,15 +5,22 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, playground")
-	n:= 42
-	fmt.Printf("%v, %T", n,n)
+	// fmt.Println("Hello, playground")
+	// n:= 42
+	// fmt.Printf("%v, %T", n,n)
 	
-	s := "this is string"
-	fmt.Println(s)
-	b := []byte(s)
-	fmt.Printf("%v,%T\n",b,b)
+	// s := "this is string"
+	// fmt.Println(s)
+	// b := []byte(s)
+	// fmt.Printf("%v,%T\n",b,b)
 	
-	grades := [3]int{97,85,93}
-	fmt.Printf("Grades : %v", grades)
+	// grades := [3]int{97,85,93}
+	// fmt.Printf("Grades : %v", grades)
+
+	a := [...]int{1,2,3}
+	b := &a // make b to be the pointer to a
+	b[1] = 5
+	fmt.Println(a)
+	fmt.Println(b)
+
 }

164f8c822c873962475c267711058995a7c2166f
xavier <xieyuehao@gmail.com>
2019-12-28 10:43:28 +1000

    slice

--- main.go
+++ main.go
@@ -33,11 +33,20 @@ func main() {
 	s1 := a1[:] //slice all elements
 	s2 := a1[3:] //slice from 4th to end
 	s3 := a1[:6] //slice first 6 elemetns
-	s4 := a1[3:6] //slice from 4th, 5th and 6th
+	s4 := a1[3:6] //slice from 4th, 5th and 6th first item is inclusive and second item is exclusive
 
 	fmt.Println("a1 is ", a1)
 	fmt.Println("s1 is ", s1)
 	fmt.Println("s2 is ", s2)
 	fmt.Println("s3 is ", s3)
 	fmt.Println("s4 is ", s4)
+
+
+	a3 := []string{"a","b","c","d","e"}
+	fmt.Println("a3 is ",a3)
+	fmt.Println("a3[:2] is ",a3[:2])
+	fmt.Println("a3[3:]... is ",a3[3:])
+	b2 := append(a3[:2], a3[3:]...)
+	fmt.Println("b2 is ",b2)
+	fmt.Println("a3 is ",a3)
 }
